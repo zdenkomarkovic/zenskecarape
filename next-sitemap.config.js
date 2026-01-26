8c0a08f3279cd765a7f67e924455663d0f88bde6
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-01-26 21:50:12 +0100

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://zenskecarape.rs", // Zameni svojim domenom
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};

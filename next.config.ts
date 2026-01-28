360b10b0bb9296ede613a6906288e977c824ca86
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-01-28 03:08:25 +0100

    .

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // Isključuje Vercel Image Optimization
     remotePatterns: [
       {
         protocol: 'https',

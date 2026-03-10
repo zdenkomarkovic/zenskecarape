42021c385b914e4c08f9e4cf32d0007725f5345a
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-03-10 23:54:44 +0100

    .

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -2,11 +2,7 @@
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '@/sanity.config'
-import { notFound } from 'next/navigation'
 
 export default function StudioPage() {
-  if (process.env.NODE_ENV === 'production' && process.env.STUDIO_ENABLED !== 'true') {
-    notFound()
-  }
   return <NextStudio config={config} />
 }
